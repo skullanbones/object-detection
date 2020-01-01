bfeaed028395bee42e0c3c65fc883459e3af0dbf
Christian Köhnenkamp <cvk5@me.com>
2020-01-01 16:21:04 +0100

    Add functions

--- scripts/filter_object.py
+++ scripts/filter_object.py
@@ -11,12 +11,6 @@ import cv2
 
 # This is needed since the notebook is stored in the object_detection folder.
 sys.path.append("..")
-
-# ## Object detection imports
-# Here are the imports from the object detection module.
-
-# In[3]:
-
 from utils import label_map_util
 from utils import visualization_utils as vis_util
 
@@ -37,7 +31,6 @@ def has_web_cam(source=0):
 # Video assets to stream
 video_files = []
 
-
 if has_web_cam(0):
   print('Using webcamera.')
   video_files = [0] # Try enable to use webcamera instead
@@ -55,60 +48,42 @@ else:
 
 # In[4]:
 
-# What model to use / download.
-MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
+def load_model():
+  # What model to use / download.
+  MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
 
-MODEL_FILE = MODEL_NAME + '.tar.gz'
-DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
-
-
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
-print("Using model: %s" % PATH_TO_CKPT)
-
-# List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
-
-NUM_CLASSES = 90
-
-
-# ## Download Model
-
-# In[5]:
-
-if not os.path.isfile(PATH_TO_CKPT):
-  opener = urllib.request.URLopener()
-  opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-  tar_file = tarfile.open(MODEL_FILE)
-  for file in tar_file.getmembers():
-    file_name = os.path.basename(file.name)
-    if 'frozen_inference_graph.pb' in file_name:
-      tar_file.extract(file, os.getcwd())
-else:
-  print("File exists: {0}".format(PATH_TO_CKPT))
-
-
-# ## Load a (frozen) Tensorflow model into memory.
-
-# In[6]:
-
-detection_graph = tf.Graph()
-with detection_graph.as_default():
-  od_graph_def = tf.GraphDef()
-  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-    serialized_graph = fid.read()
-    od_graph_def.ParseFromString(serialized_graph)
-    tf.import_graph_def(od_graph_def, name='')
-
-
-# ## Loading label map
+  MODEL_FILE = MODEL_NAME + '.tar.gz'
+  DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+  # Path to frozen detection graph. This is the actual model that is used for the object detection.
+  PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+  print("Using model: %s" % PATH_TO_CKPT)
+  # List of the strings that is used to add correct label for each box.
+  PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+  NUM_CLASSES = 90
+  ## Download Model
+  if not os.path.isfile(PATH_TO_CKPT):
+    opener = urllib.request.URLopener()
+    opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+    tar_file = tarfile.open(MODEL_FILE)
+    for file in tar_file.getmembers():
+      file_name = os.path.basename(file.name)
+      if 'frozen_inference_graph.pb' in file_name:
+        tar_file.extract(file, os.getcwd())
+  else:
+    print("File exists: {0}".format(PATH_TO_CKPT))
+  ## Load a (frozen) Tensorflow model into memory.
+  detection_graph = tf.Graph()
+  with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+      serialized_graph = fid.read()
+      od_graph_def.ParseFromString(serialized_graph)
+      tf.import_graph_def(od_graph_def, name='')
+## Loading label map
 # Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
-
-# In[7]:
-
-label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-category_index = label_map_util.create_category_index(categories)
+  label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+  categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+  category_index = label_map_util.create_category_index(categories)
 
 
 # ## Helper code
@@ -134,9 +109,8 @@ def get_display_string(
   agnostic_mode=False,
   groundtruth_box_visualization_color='black',
   skip_scores=False,
-  skip_labels=False
-):
-    # Create a display string (and color) for every box location, group any boxes
+  skip_labels=False):
+  # Create a display string (and color) for every box location, group any boxes
   # that correspond to the same location.
   box_to_display_str_map = collections.defaultdict(list)
   box_to_color_map = collections.defaultdict(str)
@@ -174,76 +148,105 @@ def get_display_string(
             strings.append(display_str)
   return strings
 
-# # Detection
-
-# In[9]:
-
-# For the sake of simplicity we will use only 2 images:
-# image1.jpg
-# image2.jpg
-# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
-PATH_TO_TEST_IMAGES_DIR = 'test_images'
-TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
-
-# Size, in inches, of the output images.
-IMAGE_SIZE = (12, 8)
 
 
-# In[10]:
+# Detection
+def run_detection():
+  # If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
+  PATH_TO_TEST_IMAGES_DIR = 'test_images'
+  TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
+  
+  # Size, in inches, of the output images.
+  IMAGE_SIZE = (12, 8)
+  
+  for videofile in video_files:
+    print("processing file {0}".format(videofile))
+  
+    try:
+      cap = cv2.VideoCapture(videofile)
+    except:
+      print("Could not open video file {0}".format(videofile))
+  
+    with detection_graph.as_default():
+      with tf.Session(graph=detection_graph) as sess:
+        while True:
+          ret, image_np = cap.read()
+          # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+          image_np_expanded = np.expand_dims(image_np, axis=0)
+          image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+          # Each box represents a part of the image where a particular object was detected.
+          boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+          # Each score represent how level of confidence for each of the objects.
+          # Score is shown on the result image, together with the class label.
+          scores = detection_graph.get_tensor_by_name('detection_scores:0')
+          classes = detection_graph.get_tensor_by_name('detection_classes:0')
+          num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+          # Actual detection.
+          try: 
+            (boxes, scores, classes, num_detections) = sess.run(
+                [boxes, scores, classes, num_detections],
+                feed_dict={image_tensor: image_np_expanded})
+          except:
+            print("got exception continue...")
+            break
+          # Visualization of the results of a detection.
+          display = get_display_string(
+            np.squeeze(boxes),
+              np.squeeze(classes).astype(np.int32),
+              np.squeeze(scores),
+              category_index,
+            )
+          if display:
+            print("scores: {0}".format(display))
+  
+          cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
+          if cv2.waitKey(25) & 0xFF == ord('n'):
+            cv2.destroyAllWindows()
+            break
+          elif cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            sys.exit()
+            break
 
-for videofile in video_files:
-  print("processing file {0}".format(videofile))
 
-  try:
-    cap = cv2.VideoCapture(videofile)
-  except:
-    print("Could not open video file {0}".format(videofile))
+def parse_arguments(lista):
+    parser = argparse.ArgumentParser(description='Object boolean filter to find objects in video files.')
+    parser.add_argument(
+        'input',
+        help='a path to a video file')
+    parser.add_argument(
+        '--model',
+        dest='model',
+        help='a path to the pb model file; the default is `model.pb`')
+    parser.add_argument(
+        '--silence-threshold',
+        dest='silence_threshold',
+        type=float,
+        help=("indicates what sample value you should treat as silence; "
+              "the default is `0.5`"))
+    parser.add_argument(
+        '--silence-min-duration',
+        dest='silence_min_duration_sec',
+        type=float,
+        help=("specifies a period of silence that must exist before video is "
+              "not copied any more; the default is `0.1`"))
+    parser.add_argument(
+        '--verbose',
+        dest='verbose',
+        action='store_true',
+        help='print more logs')
 
-  with detection_graph.as_default():
-    with tf.Session(graph=detection_graph) as sess:
-      while True:
-        ret, image_np = cap.read()
-        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-        image_np_expanded = np.expand_dims(image_np, axis=0)
-        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-        # Each box represents a part of the image where a particular object was detected.
-        boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-        # Each score represent how level of confidence for each of the objects.
-        # Score is shown on the result image, together with the class label.
-        scores = detection_graph.get_tensor_by_name('detection_scores:0')
-        classes = detection_graph.get_tensor_by_name('detection_classes:0')
-        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-        # Actual detection.
-        try: 
-          (boxes, scores, classes, num_detections) = sess.run(
-              [boxes, scores, classes, num_detections],
-              feed_dict={image_tensor: image_np_expanded})
-        except:
-          print("got exception continue...")
-          break
-        # Visualization of the results of a detection.
-        display = get_display_string(
-          np.squeeze(boxes),
-            np.squeeze(classes).astype(np.int32),
-            np.squeeze(scores),
-            category_index,
-          )
-        if display:
-          print("scores: {0}".format(display))
-        # vis_util.visualize_boxes_and_labels_on_image_array(
-        #     image_np,
-        #     np.squeeze(boxes),
-        #     np.squeeze(classes).astype(np.int32),
-        #     np.squeeze(scores),
-        #     category_index,
-        #     use_normalized_coordinates=True,
-        #     line_thickness=8)
+    parser.set_defaults(
+        model='model.pb',
+        silence_min_duration_sec=0.1,
+        silence_threshold=0.5,
+        verbose=False)
 
-        cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
-        if cv2.waitKey(25) & 0xFF == ord('n'):
-          cv2.destroyAllWindows()
-          break
-        elif cv2.waitKey(25) & 0xFF == ord('q'):
-          cv2.destroyAllWindows()
-          sys.exit()
-          break
+    args = parser.parse_args(lista)
+
+
+def main():
+    parse_arguments(sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
